12324496837f0369a351f462647ebc911bf382d9
iam-devAlhan <Alhaanahmed123@gmail.com>
2025-11-11 20:46:00 +0500

    feat: Added API Router for calling Zoom APIs

---
+++ integrations/zoom/router.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="api/v1/zoom_client/")
+
+@router.get("/auth/zoom/login")
+async def zoom_login():
+    pass
+
+@router.get("/auth/zoom/callback")
+async def zoom_callback():
+    pass
+
+
