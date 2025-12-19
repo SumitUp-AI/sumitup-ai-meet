bd3cadf5e0780fa15331a1cb42494ad9e4d6ceb7
alhaan-org <alhaanahmed@nexiatech.org>
2025-12-19 19:34:08 +0500

    fix: Added rate limiting, added schemas for action items and summarization to process summary

--- core/pipelines.py
+++ core/pipelines.py
@@ -1,51 +1,65 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 from pipelines import create_action_items_json, summarize_meeting_transcripts
+from limiter.limiter import limiter
 from pydantic import BaseModel
+from typing import List, Optional
 
 # Schema for submitting transcripts
+
+class TranscriptData(BaseModel):
+    start_time: str
+    end_time: str
+    speaker: str
+    text: str
+    
 class MeetingTranscriptPayload(BaseModel):
-    transcript: str
+    transcript: List[TranscriptData]
 
 class MeetingTranscriptOutput(BaseModel):
+    dated_at: Optional[str] = None
+    organization: Optional[str] = None
     summary: str
 
-
+class CreateActionItems(BaseModel):
+    summary: str
+    
 router = APIRouter(
     prefix="/api/v1",
-    tags=["Summarization", "Action Items API"]
+    tags=["Summarization and Action Items API"]
 )
 
-
-
-
 @router.post("/create_summary", response_model=MeetingTranscriptOutput)
-async def get_summary_from_raw_transcript(payload: MeetingTranscriptPayload):
+@limiter.limit("6/minute")
+async def get_summary_from_raw_transcript(request: Request, payload: MeetingTranscriptPayload):
     """
     This API is for generating summary from transcripts
     """
     try:
-        if payload.transcript.strip() == "":
-            raise HTTPException(status_code=400, detail="Transcripts cannot be empty")
+        combined_text = " ".join([chunk.text for chunk in payload.transcript])
         
-        summary = summarize_meeting_transcripts(payload.transcript)
+        if not payload.transcript or not combined_text.strip():
+            raise HTTPException(status_code=400, detail="Failed to process, Transcripts cannot be empty!!")
+        
+        summary = summarize_meeting_transcripts(combined_text)
         return {"summary": summary}
     
     except Exception as e:
         raise HTTPException(status_code=500, detail="Server failed to process Summary")
-    
-    
+
+
 @router.post("/create_action_items")
-async def get_action_items(summary: str):
+@limiter.limit("6/minute")
+async def get_action_items(request: Request, payload: CreateActionItems):
     """
     This API is for generating summary from transcripts
     """
     # DB logic applies here later
     try:
-        if summary.strip() == "":
+        if payload.summary.strip() == "":
             raise HTTPException(status_code=400, detail="Summary cannot be empty")
         
-        action_items = create_action_items_json(summary)
+        action_items = create_action_items_json(payload.summary)
         return JSONResponse({"items": action_items["items"]})
     
     except Exception as e:
