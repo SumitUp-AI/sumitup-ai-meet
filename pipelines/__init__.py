82c83dcad4ec97ecf5cc7a64d585348a41d42f97
alhaan-org <alhaanahmed@nexiatech.org>
2025-12-19 19:34:59 +0500

    feat: Added Rate Limiter File for custom rate limiting on every API Endpoint

--- pipelines/__init__.py
+++ pipelines/__init__.py
@@ -1,2 +1,2 @@
-from summarization import summarize_meeting_transcripts
-from action_items import create_action_items_json
+from .summarization import summarize_meeting_transcripts
+from .action_items import create_action_items_json
