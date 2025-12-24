cac6ccd3ff65c54b3051e68e3e805cee06e26bee
iam-devAlhan <alhaanahmed123@gmail.com>
2025-12-24 22:40:16 +0500

    feat: Added Auth imports, Authentication to build

--- auth/auth.py
+++ auth/auth.py
@@ -1,1 +1,22 @@
-# Authentication Work here
+from fastapi import APIRouter, HTTPException, Depends, status
+from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from datetime import datetime, timezone, timedelta
+from pwdlib import PasswordHash
+from pydantic import BaseModel
+from dotenv import load_dotenv, find_dotenv
+
+import jwt
+import os
+
+load_dotenv(find_dotenv())
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+router = APIRouter(
+    prefix="/api/v1/",
+    tags=["Authentication and Authorization"]
+)
