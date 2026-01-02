b4451f953a3f3b3e0b3a6f94ced43c8cc6517cdc
iam-devAlhan <alhaanahmed123@gmail.com>
2026-01-02 21:58:03 +0500

    feat: Defined Meeting Endpoints, Established Connections and Added All Models. Will add more further

---
+++ backend/core/meetings.py
@@ -0,0 +1,17 @@
+from fastapi import HTTPException, APIRouter, Request
+from fastapi.responses import JSONResponse
+
+router = APIRouter(
+    prefix="/api/v1/",
+    tags=["Meeting Processing and Action Items"]
+)
+
+@router.post("/create_meeting")
+async def create_meeting(request: Request):
+    pass
+
+
+@router.post("/create_physical_meeting")
+async def create_physical_meeting(request: Request):
+    pass
+
--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,19 +1,23 @@
 # Database Connection
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
-from models.models import User, Tenant
+from models.models import User, Tenant, ActionItems, Meeting, Participants, Transcripts, Team, Billing
 import asyncio
+import os
+from dotenv import load_dotenv, find_dotenv
 
-MONGO_URI = "mongodb://localhost:27017"
-DB_NAME = "test"
+load_dotenv(find_dotenv())
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "test")
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DB_NAME]
 
 async def init_db():
-    await init_beanie(db, document_models=[User, Tenant])
+    await init_beanie(db, document_models=[User, Tenant, ActionItems, Meeting, Participants, Transcripts, Team, Billing])
     
-    # This is for testing only when data is inserted
+    # This was used for testing only when data is inserted
     # if await User.count() == 0:
     #     await User(
     #         name="system",
