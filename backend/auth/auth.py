9a09435caffe1c825adebb4269722583e5ea9c66
iam-devAlhan <alhaaahmed123@gmail.com>
2026-02-08 20:04:51 +0500

    fix: Improved Authentication wth additon to refresh token and httponly cookie.
    Using httponly cookie we can secure our authentication instead of using storing token in client when server stores information in an httpOnly cookie in client which prevents XSS Attacks

--- backend/auth/auth.py
+++ backend/auth/auth.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 SECRET_KEY = os.getenv("SECRET_KEY")
+REFRESH_TOKEN_SECRET = os.getenv("REFRESH_TOKEN_SECRET")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
@@ -22,3 +23,22 @@ def decode_access_token(token: str):
         return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
     except JWTError:
         return None
+    
+def create_refresh_token(data: dict, duration: int):
+    if not REFRESH_TOKEN_SECRET:
+        raise RuntimeError("REFRESH_TOKEN_SECRET is missing or not set!")
+    
+    to_encode = data.copy()
+    # User will set the days based on that
+    # If user says Remember Me, We will implement 7 days max for refreshing otherwise 1 day.
+    expires = datetime.now(timezone.utc) + timedelta(days=duration)
+    to_encode.update({"exp": expires})
+    return jwt.encode(to_encode, REFRESH_TOKEN_SECRET, algorithm=ALGORITHM)
+
+def decode_refresh_token(refresh_token: str):
+    try:
+        return jwt.decode(refresh_token, REFRESH_TOKEN_SECRET, algorithms=[ALGORITHM])
+    except JWTError:
+        return None
+
+
