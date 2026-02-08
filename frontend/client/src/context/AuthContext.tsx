46a07d4675cf31b8d7981689427828d60e6db8ec
iam-devAlhan <alhaaahmed123@gmail.com>
2026-02-08 23:20:00 +0500

    fix: Working on client for Authentication

--- frontend/client/src/context/AuthContext.tsx
+++ frontend/client/src/context/AuthContext.tsx
@@ -56,6 +56,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
+        credentials: "include"
       });
 
       if (!res.ok) {
