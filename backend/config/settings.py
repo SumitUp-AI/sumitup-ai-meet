7189592e0c43adba67537e7af483a9552dd18be2
iam-devAlhan <alhaaahmed123@gmail.com>
2026-05-31 23:09:10 +0500

    feat: Added config for environment variables

---
+++ backend/config/settings.py
@@ -0,0 +1,37 @@
+# Config.py
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Groq Inference and HuggingFaces
+    hf_token: str
+    groq_api_key: str
+
+    # App's Secret and Refresh Token Config
+    secret_key: str
+    refresh_token_secret: str
+
+    # MongoDB Config
+    mongo_uri: str = "mongodb://localhost:27017"
+    mongodb_atlas_uri: str
+    db_name: str = "test"
+
+    # Attendee Config
+    webhook_secret: str
+    attendee_api_key: str
+
+    # Deepgram Config
+    deepgram_api_key: str
+
+    # Zoom OAuth Config
+    zoom_client_id: str
+    zoom_client_secret: str
+    zoom_redirect_uri: str = "http://localhost:3000/api/v1/zoom/callback/"
+    
+    # Google Gemini Config
+    google_api_key: str
+
+    # Cohere API Key for Reranking
+    cohere_api_key: str
+    
+settings = Settings()
+
